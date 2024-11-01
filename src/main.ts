4349048ff96ebf59de68db031862c8052d482ba8
MiguelDP4 <migueldp4@gmail.com>
2024-10-31 21:19:40 -0600

    add edit button for project and tasks

--- src/main.ts
+++ src/main.ts
@@ -32,18 +32,68 @@ const projects: Array<Project> = [
   }
 ];
 
-const projectsListDisplay = document.getElementById('projects');
+const renderEditProjectModal = (project: Project) => {
+  const body = document.getElementsByTagName('body')[0];
+  if(body) {
+    const modalContainer = document.createElement('div');
+    modalContainer.classList.add("modal-container");
+    const modalWindow = document.createElement('div');
+    modalWindow.classList.add("modal");
+    modalWindow.classList.add("project");
+    modalContainer.append(modalWindow);
+    const editTask = document.createElement('h4');
+    editTask.innerHTML = "Edit Project";
+    modalWindow.append(editTask);
+    const nameInput = document.createElement('input');
+    nameInput.type = "text";
+    nameInput.value = project.name;
+    modalWindow.append(nameInput);
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add("button-container");
+    const cancelButton = document.createElement('button');
+    cancelButton.innerHTML='Cancel';
+    const saveButton = document.createElement('button');
+    saveButton.innerHTML='Save';
+    saveButton.addEventListener('click', () => {
+      project.name = nameInput.value;
+      updateProjectsListDisplay();
+      modalContainer.remove();
+    });
+    cancelButton.addEventListener('click', () => {
+      modalContainer.remove();
+    })
+    buttonContainer.append(cancelButton);
+    buttonContainer.append(saveButton);
+    modalWindow.append(buttonContainer);
+    body.append(modalContainer);
+  }
+}
 
 const updateProjectsListDisplay = () => {
+  const projectsListDisplay = document.getElementById('projects');
   if(projectsListDisplay) {
     projectsListDisplay.innerHTML = '';
+    console.log(projects);
     projects.forEach((project) => {
       const newLi = document.createElement('li');
-      newLi.innerHTML = `Project ${project.id}`
-      newLi.addEventListener('click', () => {
+      newLi.id = `project-${project.id}`;
+      const newSpan = document.createElement('span');
+      newSpan.innerHTML = project.name;
+      newSpan.addEventListener('click', () => {
         selectedProjectIndex = project.id - 1;
         updateProjectDisplay();
       });
+      newLi.append(newSpan);
+
+      const editButton = document.createElement('button');
+      editButton.classList.add("edit-button");
+      const editImage = document.createElement('img');
+      editImage.src = '/modify-icon.svg';
+      editImage.alt = 'modify-icon';
+      editButton.addEventListener('click', () => renderEditProjectModal(project));
+      editButton.append(editImage);
+      
+      newLi.append(editButton);
       if(project.active) {
         projectsListDisplay.append(newLi);
       }
@@ -103,6 +153,14 @@ const createTaskCard = (task: Task) => {
   taskDescription.classList.add("task-description");
   taskDescription.innerHTML = task.description;
   newTaskDiv.append(taskDescription);
+  const editButton = document.createElement('button');
+  editButton.classList.add("edit-button");
+  const editImage = document.createElement('img');
+  editImage.src = '/modify-icon.svg';
+  editImage.alt = 'modify-icon';
+  editButton.addEventListener('click', () => renderEditTaskModal(task))
+  editButton.append(editImage);
+  newTaskDiv.append(editButton);
   return newTaskDiv;
 }
 
@@ -153,6 +211,46 @@ if(clearButton) clearButton.addEventListener('click', () => {
   updateProjectsListDisplay();
   if(projectDisplay) projectDisplay.innerHTML=`<h3>Create and select a project</h3>`
 })
+
+const renderEditTaskModal = (task: Task) => {
+  const body = document.getElementsByTagName('body')[0];
+  if(body) {
+    const modalContainer = document.createElement('div');
+    modalContainer.classList.add("modal-container");
+    const modalWindow = document.createElement('div');
+    modalWindow.classList.add("modal");
+    modalContainer.append(modalWindow);
+    const editTask = document.createElement('h4');
+    editTask.innerHTML = "Edit Task";
+    modalWindow.append(editTask);
+    const nameInput = document.createElement('input');
+    nameInput.type = "text";
+    nameInput.value = task.name;
+    const descriptionInput = document.createElement('textarea');
+    descriptionInput.value = task.description;
+    modalWindow.append(nameInput);
+    modalWindow.append(descriptionInput);
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add("button-container");
+    const cancelButton = document.createElement('button');
+    cancelButton.innerHTML='Cancel';
+    const saveButton = document.createElement('button');
+    saveButton.innerHTML='Save';
+    saveButton.addEventListener('click', () => {
+      task.name = nameInput.value;
+      task.description = descriptionInput.value;
+      updateProjectDisplay();
+      modalContainer.remove();
+    });
+    cancelButton.addEventListener('click', () => {
+      modalContainer.remove();
+    })
+    buttonContainer.append(cancelButton);
+    buttonContainer.append(saveButton);
+    modalWindow.append(buttonContainer);
+    body.append(modalContainer);
+  }
+}
 
 updateProjectsListDisplay();
 updateProjectDisplay();
